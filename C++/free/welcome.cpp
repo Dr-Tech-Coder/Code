b5e94347cf687e387192be27877ad86a0c9e4810
Shashank Verma <shashank.verma2002@gmail.com>
2020-08-30 13:50:04 +0800

    Welcome C++
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

---
+++ C++/free/welcome.cpp
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2020, Shashank Verma <shashank.verma2002@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ */
+
+#include <iostream>
+
+/**
+ * This is the main method of C++ where the code is executed
+ */
+int main() {
+
+    // This is the line to print out text.
+    // cout = c out (explains itself about something 'out')
+    // endl = end line (which is the char '\n' to shift to next line)
+    // '<<' = to join the out (cout) stream
+    std::cout << "Hello C++ !" << std::endl;
+
+    return 0;
+}
