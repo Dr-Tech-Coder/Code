5ce356a530a0f475f785a86c2691eeba44f4574c
Shashank Verma <shashank.verma2002@gmail.com>
2020-08-30 13:11:08 +0800

    Initialize Java code
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

---
+++ Java/free/Welcome.java
@@ -0,0 +1,21 @@
+package Java.free;
+
+/**
+ * This is a example class named 'Welcome'
+ * The file name and the class name should be same.
+ */
+public class Welcome {
+
+    /**
+     * This is the main method.
+     * This method executes the code and run the program.
+     * 
+     * This main method is supposed to be present inside a class.
+     * 
+     * Notice that the method and class have curly braces '{}' (part of syntax)
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello Java !");     // A basic statement/line to print out some text
+        // Each line should end with a ';' just like a fullstop '.' in english
+    }
+}
